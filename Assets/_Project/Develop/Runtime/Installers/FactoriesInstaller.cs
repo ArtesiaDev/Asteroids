19a4db758284e9b96ac9d250c53c42ac0ad9f1bb
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-21 18:30:01 +0300

    add: installers for gamestatemachine

---
+++ Assets/_Project/Develop/Runtime/Installers/FactoriesInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Develop.Runtime.Infrastructure.Factories;
+using Zenject;
+
+namespace Develop.Runtime.Installers
+{
+    public sealed class FactoriesInstaller: MonoInstaller
+    {
+        public override void InstallBindings() =>
+            BindFactories();
+
+        private void BindFactories()
+        {
+            Container.Bind<StateFactory>().AsSingle().NonLazy();
+        }
+    }
+}
