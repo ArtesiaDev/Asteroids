855aea508b7d42e993e4a5396886c6dc056da7c0
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-24 20:59:21 +0300

    add: Mobile and Standalone input

---
+++ Assets/_Project/Develop/Runtime/Services/Input/StandaloneInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Develop.Runtime.Services.Input
+{
+    public class StandaloneInput : Input
+    {
+        private const string HORIZONTAL = "Horizontal";
+
+        public override bool Move =>
+            UnityEngine.Input.GetKey(KeyCode.UpArrow) || UnityEngine.Input.GetKey(KeyCode.W);
+
+        public override float Steer =>
+            UnityEngine.Input.GetAxis(HORIZONTAL);
+
+        public override bool BulletShoot =>
+            UnityEngine.Input.GetKey(KeyCode.E);
+
+        public override bool LaserShoot =>
+            UnityEngine.Input.GetKey(KeyCode.Q);
+    }
+}
