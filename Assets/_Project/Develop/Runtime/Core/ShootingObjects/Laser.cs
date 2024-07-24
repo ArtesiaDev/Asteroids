631c3b6e0076cdd6c8034a9e9a05bd24d1e776a6
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-24 19:43:33 +0300

    add: ShootingJObjects (bullet, laser)

---
+++ Assets/_Project/Develop/Runtime/Core/ShootingObjects/Laser.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Develop.Runtime.Core.ShootingObjects
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(CapsuleCollider2D))]
+    public class Laser: MonoBehaviour
+    {
+        public void Destroy(float time) =>
+            Destroy(gameObject, time);
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
