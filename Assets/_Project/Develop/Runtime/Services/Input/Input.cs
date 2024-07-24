33dfc1da177a1e9e00d6131629b6c6f39a03820c
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-24 20:58:38 +0300

    add: abstract Input

---
+++ Assets/_Project/Develop/Runtime/Services/Input/Input.cs
@@ -0,0 +1,13 @@
+﻿namespace Develop.Runtime.Services.Input
+{
+    public abstract class Input: IInput
+    {
+        public virtual bool Move { get; }
+        
+        public virtual float Steer { get; }
+        
+        public virtual bool BulletShoot { get; }
+        
+        public virtual bool LaserShoot { get; }
+    }
+}
