579aec69cc82903b21d6584f24f995b1cf637838
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-26 14:55:48 +0300

    add: EnemyConfig

---
+++ Assets/_Project/Develop/Runtime/Core/Configs/EnemyConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Develop.Runtime.Core.Configs
+{
+    [CreateAssetMenu(fileName = "EnemyConfig", menuName = "Configs/EnemyConfig", order = 0)]
+    public class EnemyConfig : ScriptableObject
+    {
+        [field: Header("Asteroid")]
+        [field: SerializeField]
+        public float SpawnInterval { get; private set; }
+        
+        [field: SerializeField] 
+        public float HugeSpawnRadius { get; private set; }
+        
+        [field: SerializeField] 
+        public float MedSpawnRadius { get; private set; }
+        
+        [field: SerializeField] 
+        public float SmallSpawnRadius { get; private set; }
+    }
+}
