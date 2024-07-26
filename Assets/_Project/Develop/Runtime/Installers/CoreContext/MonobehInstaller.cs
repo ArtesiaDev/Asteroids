5f3d9187a2112a8e17b21fbc79b8bf1b9058164c
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-26 19:37:42 +0300

    add: MonobehInstaller

---
+++ Assets/_Project/Develop/Runtime/Installers/CoreContext/MonobehInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Develop.Runtime.Core.Spawn;
+using UnityEngine;
+using Zenject;
+
+namespace Develop.Runtime.Installers.CoreContext
+{
+    public sealed class MonobehInstaller : MonoInstaller
+    {
+        public override void InstallBindings() =>
+            BindMonobehavior<AsteroidSpawner>();
+
+        private void BindMonobehavior<T>() where T : MonoBehaviour
+        {
+            var instance = FindObjectOfType<T>();
+
+            if (instance == null)
+                Debug.LogWarning($"Instance of type: {typeof(T)}  not found.");
+
+            Container.BindInterfacesAndSelfTo<T>().FromInstance(instance).AsSingle();
+        }
+    }
+}
