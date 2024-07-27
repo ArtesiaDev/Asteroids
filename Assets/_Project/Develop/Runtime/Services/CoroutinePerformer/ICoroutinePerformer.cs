7e98e54452194b31749361638f0409b7ca179800
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-07-27 20:35:45 +0300

    add: CoroutinePerformer

---
+++ Assets/_Project/Develop/Runtime/Services/CoroutinePerformer/ICoroutinePerformer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Develop.Runtime.Services.CoroutinePerformer
+{
+    public interface ICoroutinePerformer
+    {
+        public Coroutine StartPerform(IEnumerator coroutine);
+
+        public void StopPerform(Coroutine coroutine);
+    }
+}
