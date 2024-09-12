8ddf8d4b96d49c8dfb7941cf31081b593683ee2b
ArtesiaDev <malashenkoivan2002@gmail.com>
2024-09-12 18:24:38 +0300

    fix IronSource initialize bug

--- Assets/_Project/Develop/Backend/Ads/IronSourceAdsInitialize.cs
+++ Assets/_Project/Develop/Backend/Ads/IronSourceAdsInitialize.cs
@@ -2,10 +2,7 @@
 {
     public class IronSourceAdsInitialize
     {
-#if UNITY_ANDROID
         private const string APP_KEY = "1f7d5ea95";
-#endif
-
         public void Initialize()
         {
             IronSource.Agent.validateIntegration();
